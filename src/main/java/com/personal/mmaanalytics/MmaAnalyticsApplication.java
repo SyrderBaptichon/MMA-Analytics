0c5e1dc59f1289cad6d9dc3cf061d363ccb6b58d
SyrderBaptichon <dorybaptichon@gmail.com>
2025-11-10 23:41:10 +0100

    project setup

---
+++ src/main/java/com/personal/mmaanalytics/MmaAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.personal.mmaanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmaAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/mmaanalytics/MmaAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.mmaanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmaAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
